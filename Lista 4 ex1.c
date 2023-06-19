58b8266ef4c4234fbad2bad54c0de37dd81ee78a
Eirado <111468283+Eirado@users.noreply.github.com>
2023-06-19 12:37:12 -0300

    Add files via upload

---
+++ Lista 4 ex1.c
@@ -0,0 +1,35 @@
+/*1. Escreva uma função mm que receba um vetor inteiro v[0..n-1] e os endereços de duas variáveis inteiras, 
+digamos min e max, e deposite nessas variáveis o valor de um elemento mínimo e o valor de um elemento máximo 
+do vetor.Escreva também uma função main que use a função mm.*/
+
+
+#include <stdio.h>
+#define TamanhoVetor 5
+
+
+void mm(int v[], int n, int* min, int* max) {
+    int i = 0;
+    *min = v[0];
+    *max = v[0];
+    
+    for (i = 1; i < n; i++) {
+        if (v[i] < *min) {
+            *min = v[i];
+        }
+        if (v[i] > *max) {
+            *max = v[i];
+        }
+    }
+}
+
+int main() {
+    int v[] = {6, 4, 2, 10, 2};
+    int n = TamanhoVetor; 
+    int min, max;
+
+    mm(v, n, &min, &max);
+
+    printf("\nO valor minimo eh: %d e O valor maximo eh: %d \n", min, max);
+
+    return 0;
+}
---
+++ Lista 4 ex2.c
@@ -0,0 +1,33 @@
+/*2. Escreva uma função que recebe como parâmetros um vetor de inteiros v, o número de elementos dele N e 
+ponteiros para variáveis nas quais devem ser armazenados os valores máximo e mínimo do vetor. Sua assinatura deve 
+ser:void maximoMinimo(int *v, int N, int *maximo, int *minimo);*/
+
+#include <stdio.h>
+#define TamanhoVetor 5
+
+void maximoMinimo(int *v, int N, int *maximo, int *minimo) {
+    int i = 1;
+    *maximo = v[0];
+    *minimo = v[0];
+
+    for (i = 1; i < N; i++) {
+        if (v[i] > *maximo) {
+            *maximo = v[i];
+        }
+        if (v[i] < *minimo) {
+            *minimo = v[i];
+        }
+    }
+}
+
+int main() {
+    int v[] = {3, 33, 23, 35, 2352};
+    int N = TamanhoVetor; 
+    int maximo, minimo;
+
+    maximoMinimo(v, N, &maximo, &minimo);
+
+    printf("\nO valor minimo eh: %d e O valor maximo eh: %d \n", minimo, maximo);
+
+    return 0;
+}
